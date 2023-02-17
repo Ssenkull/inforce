c7c0b8b5970a16b9a6269a714055693145c09a3c
Ssenkull <bogdan.zbarazhskiy@gmail.com>
2023-02-17 14:36:19 +0200

    first commit

---
+++ src/components/AddButton/AddButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from '@mui/material/Button';
+
+const AddButton = () => {
+    return (
+      <Button variant="contained" onClick>Add</Button>
+    );
+};
+
+export default AddButton;
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './Modal.css'
+
+const Modal = (active, setActive) => {
+    return (
+        <div className={active ? 'modal active': 'modal'} onClick={() => setActive(false)}>
+            <div className="modal_content" onClick={e => e.stopPropagation()}>
+
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
